8166d7f13dc9447b1e351e40739a2efd42821a9f
A01742269-Kyan <A01742269@itesm.mx>
2021-08-20 20:42:27 +0000

    Maximo

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    Ido = int(input("¿Tienes identificación oficial? (s/n): "))
+
+    if edad<18 or edad>=18:
+        if edad<18:
+           print("No cumples requisitos")
+        elif edad>=18:
+            print("¿Tienes identificación oficial? (s/n): ")
 
 if __name__ == '__main__':
     main()
